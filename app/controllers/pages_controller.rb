def424220ee040412d2fbd843be51a513f05c0df
Joseph Chow <greengiraffedesign@gmail.com>
2017-06-10 14:20:00 +0000

    Added videos and staff pages links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,9 @@ class PagesController < ApplicationController
   def about
   end
   
+  def staff
+  end
+  
   def pastevents
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  root to: 'pages#home'
  get 'about', to: 'pages#about'
+ get 'staff', to: 'pages#staff'
  get 'upcomingevents', to: 'pages#upcomingevents'
  get 'pastevents', to: 'pages#pastevents'
  resources :contacts
